dd48a347806d21e69561ad4b6b3d21833c4e9a23
kishiamy <kishiamy84@gmail.com>
2013-11-04 20:00:55 +0100

    setup rails 4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BestBeer::Application.config.secret_key_base = 'b45af86991218fe83a8b9dc30c757d15873d5cf18e2836884c4fe49c58488d4e08bfcc86cdd266a8a9f96cbf9dab5c4b59d78e4832f4ac8830ff7c5f641d4240'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BestBeer::Application.config.session_store :cookie_store, key: '_best_beer_session'
