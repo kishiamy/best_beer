d7d5991539cc3576cffc4ad5f8aefe36ded9c4cb
kishiamy <kishiamy84@gmail.com>
2013-11-10 00:34:50 +0100

    add haml, controller, method and view_#1

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -2,7 +2,7 @@ class Beer < ActiveRecord::Base
   validates :obdb_id, uniqueness: true, presence: true
 
   def add_vote
-    votes.present? ? votes +=1 : votes = 1
+    votes.present? ? self.votes +=1 : self.votes = 1
     self.save
   end
 end
