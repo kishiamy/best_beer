113d6aa43795659e10f0de9221ed438c0079c1eb
kishiamy <kishiamy84@gmail.com>
2013-11-09 23:05:24 +0100

    create beers table, config route for votes add rspec

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 BestBeer::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -11,8 +9,11 @@ BestBeer::Application.routes.draw do
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+     resources :beers, only: [:show, :index] do
+       member do
+         post 'vote'
+       end
+     end
 
   # Example resource route with options:
   #   resources :products do
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131104191944) do
+ActiveRecord::Schema.define(version: 20131104204627) do
 
   create_table "beers", force: true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20131104191944) do
     t.string   "obdb_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "votes"
   end
 
 end
