d6f93a476bbb7f731c92dedabe48e3433a500605
kishiamy <kishiamy84@gmail.com>
2013-11-11 20:47:04 +0100

    add factory girl gem

--- spec/controllers/beers_controller_spec.rb
+++ spec/controllers/beers_controller_spec.rb
@@ -2,12 +2,12 @@ require 'spec_helper'
 
 describe BeersController do
 
-  describe "#vote" do
-    let(:beer) { double "Bita beer" }
+  describe " POST vote" do
+    let(:beer) { FactoryGirl.create(:beer) }
     it "increase vote count" do
       expect(Beer).to receive(:find).with("1").and_return(beer)
-      expect(beer).to receive(:vote) 
-      post :vote, id: 1
+      expect(Beer.any_instance).to receive(:vote) 
+      post :vote, obdb_id: 1
       expect(response).to redirect_to(root_path)
     end
   end
