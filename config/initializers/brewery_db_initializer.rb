8e3877be66906b9aec9520d83e232879a88754e7
kishiamy <kishiamy84@gmail.com>
2014-01-28 21:10:37 +0100

    * add brewety_db.yml.example * set api key to heroku for production environment

--- config/initializers/brewery_db_initializer.rb
+++ config/initializers/brewery_db_initializer.rb
@@ -1,3 +1,4 @@
 APP_CONFIG = YAML.load_file("config/brewery_db.yml")
+APP_CONFIG ||= { production: { api_key: ENV["API_KEY"]  }  }
 
 BestBeer::Application.config.api_key = APP_CONFIG[Rails.env]['api_key']
